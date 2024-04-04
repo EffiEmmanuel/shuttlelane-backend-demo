53f55bcc7f1eed11a21c0487e93d7aa7ee26165d
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-04-04 20:52:03 +0100

    UPDATE: Add CORS origin

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,9 +2,14 @@ import express from "express";
 import cors from "cors";
 
 export default async function expressLoader(app) {
+  app.use(
+    cors({
+      origin: "https://shuttlelane-frontend-demo.vercel.app/", // Replace with your Vercel frontend URL
+      optionsSuccessStatus: 200, // optional
+    })
+  );
   app.enable("trust proxy");
   app.use(express.json({ limit: "25mb" }));
-  app.use(cors());
   app.use(express.urlencoded({ limit: "25mb", extended: false }));
   return app;
 }
