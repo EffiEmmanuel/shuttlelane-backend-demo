b270f0e8e43d5b03958f4ed00ab2c343c7583497
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-04-04 14:07:20 +0100

    UPDATE: Demo push

--- src/util/twilio.js
+++ src/util/twilio.js
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import twilio from "twilio";
 
-const TWILIO_ACCOUNT_SID = "AC3319551d170c34d5e479c10b28e5ffef";
-const TWILIO_AUTH_TOKEN = "de4d267b8710a4ceb4a1f2e293a6bf68";
-const TWILIO_PHONE_NUMBER = "+447429738615";
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
 export async function sendSMS(phone, message) {
   const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
