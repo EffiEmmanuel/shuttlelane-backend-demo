bee4ba869f32da143350b5b74d41be506cb77f44
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-04-04 14:15:37 +0100

    UPDATE: Demo push

--- src/util/twilio.js
+++ src/util/twilio.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import twilio from "twilio";
 
+// TO-DO: Remove Twilio Secrets
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
