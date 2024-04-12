e83abf78ad53e98666a45df731836a0924921748
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-04-12 20:26:38 +0100

    UPDATE: Added timestamps to booking model

--- src/model/booking.model.js
+++ src/model/booking.model.js
@@ -2,158 +2,161 @@ import mongoose from "mongoose";
 import shortid from "shortid";
 
 // SCHEMA: This schema serves as a "Parent Class" for all bookings which includes Airport Transfers, Car Hire and Priority Pass Bookings
-const bookingSchema = new mongoose.Schema({
-  booking: {
-    type: mongoose.Types.ObjectId,
-    refPath: "bookingSchemaType", // Dynamically reference the model based on the bookingType
-  },
+const bookingSchema = new mongoose.Schema(
+  {
+    booking: {
+      type: mongoose.Types.ObjectId,
+      refPath: "bookingSchemaType", // Dynamically reference the model based on the bookingType
+    },
 
-  bookingSchemaType: {
-    type: String,
-    enum: [
-      "AirportTransferBooking",
-      "PriorityPassBooking",
-      "CarRentalBooking",
-      "VisaOnArrivalBooking",
-    ],
-    required: true,
-  },
+    bookingSchemaType: {
+      type: String,
+      enum: [
+        "AirportTransferBooking",
+        "PriorityPassBooking",
+        "CarRentalBooking",
+        "VisaOnArrivalBooking",
+      ],
+      required: true,
+    },
 
-  bookingType: {
-    type: String,
-    enum: ["Airport", "Car", "Priority", "Visa"],
-    required: true,
-  },
+    bookingType: {
+      type: String,
+      enum: ["Airport", "Car", "Priority", "Visa"],
+      required: true,
+    },
 
-  bookingStatus: {
-    type: String,
-    enum: [
-      "Not yet assigned",
-      "Awaiting response",
-      "Scheduled",
-      "Cancelled",
-      "Completed",
-      "Ongoing",
-    ],
-    default: "Not yet assigned",
-  },
+    bookingStatus: {
+      type: String,
+      enum: [
+        "Not yet assigned",
+        "Awaiting response",
+        "Scheduled",
+        "Cancelled",
+        "Completed",
+        "Ongoing",
+      ],
+      default: "Not yet assigned",
+    },
 
-  bookingReference: {
-    type: String,
-  },
+    bookingReference: {
+      type: String,
+    },
 
-  bookingCurrency: {
-    type: mongoose.Types.ObjectId,
-    ref: "Currency",
-  },
+    bookingCurrency: {
+      type: mongoose.Types.ObjectId,
+      ref: "Currency",
+    },
 
-  bookingTotal: {
-    type: String,
-  },
+    bookingTotal: {
+      type: String,
+    },
 
-  paymentId: {
-    type: mongoose.Types.ObjectId,
-    ref: "Payment",
-  },
+    paymentId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Payment",
+    },
 
-  // The following fields cover the passenger's details - (By default, these are guests on the website and on the mobile app)
-  title: {
-    type: String,
-  },
+    // The following fields cover the passenger's details - (By default, these are guests on the website and on the mobile app)
+    title: {
+      type: String,
+    },
 
-  firstName: {
-    type: String,
-  },
+    firstName: {
+      type: String,
+    },
 
-  lastName: {
-    type: String,
-  },
+    lastName: {
+      type: String,
+    },
 
-  email: {
-    type: String,
-    trim: true,
-    match: /.+\@.+\..+/,
-  },
+    email: {
+      type: String,
+      trim: true,
+      match: /.+\@.+\..+/,
+    },
 
-  mobile: {
-    type: String,
-  },
+    mobile: {
+      type: String,
+    },
 
-  // If booking was made by an authenticated user (From the mobile app)
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
+    // If booking was made by an authenticated user (From the mobile app)
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
 
-  // THE FOLLOWING INFORMATION ARE NOT REQUIRED AT INITIAL DOCUMENT CREATION
-  // The following fields contain information about the DRIVER assigned to a particular booking
-  assignedDriver: {
-    type: mongoose.Types.ObjectId,
-    ref: "Driver",
-  },
+    // THE FOLLOWING INFORMATION ARE NOT REQUIRED AT INITIAL DOCUMENT CREATION
+    // The following fields contain information about the DRIVER assigned to a particular booking
+    assignedDriver: {
+      type: mongoose.Types.ObjectId,
+      ref: "Driver",
+    },
 
-  driverJobWasSentTo: {
-    type: mongoose.Types.ObjectId,
-    ref: "Driver",
-  },
+    driverJobWasSentTo: {
+      type: mongoose.Types.ObjectId,
+      ref: "Driver",
+    },
 
-  isAssignedToDriver: {
-    type: Boolean,
-    default: false,
-  },
+    isAssignedToDriver: {
+      type: Boolean,
+      default: false,
+    },
 
-  hasDriverAccepted: {
-    type: Boolean,
-    default: false,
-  },
+    hasDriverAccepted: {
+      type: Boolean,
+      default: false,
+    },
 
-  hasDriverDeclined: {
-    type: Boolean,
-    default: false,
-  },
+    hasDriverDeclined: {
+      type: Boolean,
+      default: false,
+    },
 
-  //   The following fields contain information about the VENDOR assigned to a particular booking
-  assignedVendor: {
-    type: mongoose.Types.ObjectId,
-    ref: "Vendor",
-  },
+    //   The following fields contain information about the VENDOR assigned to a particular booking
+    assignedVendor: {
+      type: mongoose.Types.ObjectId,
+      ref: "Vendor",
+    },
 
-  isAssignedToVendor: {
-    type: Boolean,
-    default: false,
-  },
+    isAssignedToVendor: {
+      type: Boolean,
+      default: false,
+    },
 
-  vendorJobWasSentTo: {
-    type: String,
-  },
+    vendorJobWasSentTo: {
+      type: String,
+    },
 
-  hasVendorAccepted: {
-    type: Boolean,
-    default: false,
-  },
+    hasVendorAccepted: {
+      type: Boolean,
+      default: false,
+    },
 
-  hasVendorDeclined: {
-    type: Boolean,
-    default: false,
-  },
+    hasVendorDeclined: {
+      type: Boolean,
+      default: false,
+    },
 
-  vendorAssignedDriver: {
-    type: mongoose.Types.ObjectId,
-    ref: "VendorDriver",
-  },
+    vendorAssignedDriver: {
+      type: mongoose.Types.ObjectId,
+      ref: "VendorDriver",
+    },
 
-  // The following fields contain information about the car assigned to a particular booking
-  // Usually for situations where a VENDOR is assigned to a booking ONLY
-  assignedCar: {
-    type: mongoose.Types.ObjectId,
-    ref: "VendorFleet",
-  },
+    // The following fields contain information about the car assigned to a particular booking
+    // Usually for situations where a VENDOR is assigned to a booking ONLY
+    assignedCar: {
+      type: mongoose.Types.ObjectId,
+      ref: "VendorFleet",
+    },
 
-  // This field holds how much the driver / vendor is being paid for a particular booking
-  bookingRate: {
-    type: String,
+    // This field holds how much the driver / vendor is being paid for a particular booking
+    bookingRate: {
+      type: String,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const BookingModel = mongoose.model("Booking", bookingSchema);
 export default BookingModel;
